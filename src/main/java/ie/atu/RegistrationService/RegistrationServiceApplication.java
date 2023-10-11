1bad067f9f223bdcff754a1b350ec862caced267
Enoch Abiodun <g00380316@atu.ie>
2023-10-11 10:36:33 +0100

    Second Boot Application

---
+++ src/main/java/ie/atu/RegistrationService/RegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.RegistrationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
+
+}
